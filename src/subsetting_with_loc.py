396544a98d7d42d3848f8c6622eef4d7f2a1a460
Samf33 <144719150+Samf33@users.noreply.github.com>
2025-10-27 08:42:33 -0400

    passed

--- src/subsetting_with_loc.py
+++ src/subsetting_with_loc.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 def subsetting_with_loc():
     df = pd.read_csv('src/municipal.tsv', sep='\t', index_col=0)
-    pass
+    return df.loc["Akaa": "Äänekoski", ["Population", "Share of Swedish-speakers of the population, %","Share of foreign citizens of the population, %"]]
 
 def main():
-    subsetting_with_loc()
+    print(subsetting_with_loc())
 
 if __name__ == "__main__":
     main()
